9650751af92d4fcbf0d9745b7a88d9b5e01e6273
string339 <singhdraken@gmail.com>
2026-08-26 19:43:58 +0530

    Add M5 Student instance vs static

---
+++ src/main/java/oop/class_problems/StudentTest.java
@@ -0,0 +1,29 @@
+package oop.class_problems;
+
+class Student {
+    String name;
+    int attendance;
+
+    static String collegeName = "SRM Institute of Science and Technology";
+    static int studentCount = 0;
+
+    Student(String name, int attendance) {
+        this.name = name;
+        this.attendance = attendance;
+        studentCount++;
+    }
+
+    static void printCollegeInfo() {
+        System.out.println(collegeName);
+        System.out.println("Students created: " + studentCount);
+    }
+}
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student s1 = new Student("Ravi", 90);
+        Student s2 = new Student("Anitha", 85);
+
+        Student.printCollegeInfo();
+    }
+}
